764d53f75a6fcbef13fe5777abd1dc98988b360a
Paulo <pshenrique2015@gmail.com>
2024-01-19 19:11:26 -0300

    [add]-mudanças em atribuições detalhes.

--- src/app/atribuicoes/atribuicoes-detalhes/atribuicoes-detalhes.component.ts
+++ src/app/atribuicoes/atribuicoes-detalhes/atribuicoes-detalhes.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Atribuicao } from '../atribuicao';
+import { ReferenciaObjeto } from '../../compartilhado/referencia-objeto';
 
 @Component({
   selector: 'app-atribuicoes-detalhes',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './atribuicoes-detalhes.component.scss'
 })
 export class AtribuicoesDetalhesComponent {
-
+  atribuicao: Atribuicao = new Atribuicao("1", "Aprovar requisição", "Aprove a descrição conforme dito.", 
+  new ReferenciaObjeto("2", "Paulo Silva"), 0, new Date(2024, 1, 1), false, 
+  new ReferenciaObjeto("566", "Access Point"), new Date(2024, 1, 1), 
+  new ReferenciaObjeto("3", "Paulo Silva"), new ReferenciaObjeto("4", "Paulo Silva"), 
+  new Date(2024, 1, 1))
 }
