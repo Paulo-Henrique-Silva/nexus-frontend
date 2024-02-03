3b7d74022e80972503d72f934f64d09a6d79a49f
Paulo <pshenrique2015@gmail.com>
2024-02-03 14:55:51 -0300

    [add]-campos de formulário adicionar.

--- src/app/componentes/componentes-adicionar/componentes-adicionar.component.ts
+++ src/app/componentes/componentes-adicionar/componentes-adicionar.component.ts
@@ -1,10 +1,66 @@
 import { Component } from '@angular/core';
+import { NexusFormulario } from '../../compartilhado/models/nexus-formulario';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MensagensValidacaoService } from '../../compartilhado/services/mensagens-validacao/mensagens-validacao.service';
+import { SessaoService } from '../../compartilhado/services/sessao/sessao.service';
+import { LocalizacoesService } from '../../localizacoes/localizacoes.service';
+import { LocalizacaoEnvio } from '../../localizacoes/models/localizacao-envio';
+import { AuthService } from '../../login/auth/auth.service';
 
 @Component({
   selector: 'app-componentes-adicionar',
   templateUrl: './componentes-adicionar.component.html',
   styleUrl: './componentes-adicionar.component.scss'
 })
-export class ComponentesAdicionarComponent {
+export class ComponentesAdicionarComponent extends NexusFormulario {
+  constructor(
+    authService : AuthService, 
+    formBuilder: FormBuilder,
+    router: Router,
+    mensagemValidacaoService: MensagensValidacaoService,
+    activatedRoute: ActivatedRoute,
+    snackBar: MatSnackBar,
+    usuarioSessaoService: SessaoService,
+    private localizacaoService: LocalizacoesService,
+  ) {
+    super(authService, formBuilder, router, mensagemValidacaoService, activatedRoute, 
+      snackBar, usuarioSessaoService);
+      
+    this.formulario = this.formBuilder.group({
+      numeroSerie: ['', Validators.required],
+      marca: ['', Validators.required],
+      modelo: ['', Validators.required],
+      tipo: ['', Validators.required],
+      localizacao: ['', Validators.required],
+      dataAquisicao: ['', Validators.required],
+    })
+  }
 
+  override onSubmit(): void {
+    this.carregando = true;
+    const nome: string = this.formulario.get('nome')?.value;
+    const descricao: string = this.formulario.get('descricao')?.value;
+
+    const projetoUID = this.sessaoService.projetoSelecionado.uid;
+    
+    const localizacao: LocalizacaoEnvio = {
+      nome: nome,
+      descricao: descricao,
+      projetoUID: projetoUID
+    };
+    
+    this.localizacaoService.adicionar(localizacao)
+      .subscribe({
+        next: () => {
+          this.mostrarSnackBarOk('Localização adicionada com sucesso!');
+          this.carregando = false;
+          this.router.navigate(['/ativos/localizacoes']);
+        },
+        error: () => {
+          this.mostrarSnackBarOk('Um erro inesperado aconteceu!');
+        }
+      })
+    }
 }
