20a35c0497c66c7e8db548470bb776a016f9a441
Paulo <pshenrique2015@gmail.com>
2024-02-04 20:01:04 -0300

    [fix]-correção de sidenav

--- src/app/componentes/componentes-buscar/componentes-buscar.component.ts
+++ src/app/componentes/componentes-buscar/componentes-buscar.component.ts
@@ -10,6 +10,30 @@ import { ComponentesService } from '../componentes.service';
   styleUrl: './componentes-buscar.component.scss'
 })
 export class ComponentesBuscarComponent extends NexusBuscar {
+
+  override colunas: string[] = [ 
+    'nome',
+    'numeroSerie',
+    'localizacao',
+    'status',
+    'tipo',
+    'marca',
+    'modelo',
+    'atualizadoPor', 'dataUltimaAtualizacao',
+    'usuarioCriador', 'dataCriacao'
+  ];
+  override colunasMostrarComo : string[] = [ 
+    'Nome',
+    'Número de Série',
+    'Localização',
+    'Status',
+    'Tipo',
+    'Marca',
+    'Modelo',
+    'Atualizado por', 'Data Última Atualização',
+    'Usuário Criador', 'Data Criação'
+  ];
+
   constructor(
     snackBar: MatSnackBar,
     sessaoService: SessaoService,
