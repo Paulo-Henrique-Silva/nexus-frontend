3d527219734a2b9bf3b64c978f7941e8cd25de21
Paulo <pshenrique2015@gmail.com>
2024-02-08 19:40:31 -0300

    [add]-buscar manutenções.

--- src/app/manutencoes/manutencoes-acoes/manutencoes-acoes.component.ts
+++ src/app/manutencoes/manutencoes-acoes/manutencoes-acoes.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { take, switchMap, EMPTY } from 'rxjs';
+import { DialogDeletarComponent } from '../../compartilhado/dialog-deletar/dialog-deletar.component';
+import { ReferenciaObjeto } from '../../compartilhado/models/referencia-objeto';
+import { SessaoService } from '../../compartilhado/services/sessao/sessao.service';
+import { SoftwaresService } from '../../softwares/softwares.service';
 
 @Component({
-  selector: 'app-manutencoes-acoes',
+  selector: 'nexus-manutencoes-acoes',
   templateUrl: './manutencoes-acoes.component.html',
   styleUrl: './manutencoes-acoes.component.scss'
 })
 export class ManutencoesAcoesComponent {
+  @Input()
+  objeto: ReferenciaObjeto = new ReferenciaObjeto();
 
+  @Output() 
+  deletou = new EventEmitter<any>();
+
+  @Output() 
+  fechou = new EventEmitter<any>();
+
+  perfil: ReferenciaObjeto = new ReferenciaObjeto();
+
+  constructor(
+    private service: SoftwaresService,
+    sessaoService: SessaoService,
+    private snackbar: MatSnackBar,
+    private dialog: MatDialog
+  ) {
+    this.perfil = sessaoService.perfilSelecionado;
+  }
+
+  deletarObjeto(): void {
+    const dialogExcluir = this.dialog.open(DialogDeletarComponent, {
+      data: this.objeto.nome
+    });
+
+    dialogExcluir.afterClosed()
+    .pipe(
+      take(1),
+      switchMap(deletou => {
+          return deletou ? this.service.deletar(this.objeto.uid) : EMPTY;
+      })
+    )
+    .subscribe({
+      next: () => {
+        //emite para carregar a tabela novamente.
+        this.deletou.emit();
+        this.mostrarSnackBarOk('Software deletado com sucesso!');
+      },
+      error: () => this.mostrarSnackBarOk('Não foi possível excluir!')
+    })
+  }
+
+  mostrarSnackBarOk(texto: string): void {
+    this.snackbar.open(texto, 'Ok')._dismissAfter(3000);
+  }
+
+  fecharAcoes(): void {
+    this.fechou.emit();
+  }
 }
--- src/app/manutencoes/manutencoes-buscar/manutencoes-buscar.component.ts
+++ src/app/manutencoes/manutencoes-buscar/manutencoes-buscar.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { NexusBuscar } from '../../compartilhado/models/nexus-buscar';
+import { SessaoService } from '../../compartilhado/services/sessao/sessao.service';
+import { SoftwaresService } from '../../softwares/softwares.service';
+import { ManutencoesService } from '../manutencoes.service';
 
 @Component({
   selector: 'app-manutencoes-buscar',
   templateUrl: './manutencoes-buscar.component.html',
   styleUrl: './manutencoes-buscar.component.scss'
 })
-export class ManutencoesBuscarComponent {
+export class ManutencoesBuscarComponent extends NexusBuscar {
 
+  override colunas: string[] = [ 
+    'nome',
+    'componente',
+    'atualizadoPor', 'dataUltimaAtualizacao',
+    'usuarioCriador', 'dataCriacao'
+  ];
+  override colunasMostrarComo : string[] = [ 
+    'Nome',
+    'Componente',
+    'Atualizado por', 'Data Última Atualização',
+    'Usuário Criador', 'Data Criação'
+  ];
+
+  constructor(
+    snackBar: MatSnackBar,
+    sessaoService: SessaoService,
+    service: ManutencoesService
+  ) {
+    super(snackBar, sessaoService);
+    this.service = service;
+  }
 }
