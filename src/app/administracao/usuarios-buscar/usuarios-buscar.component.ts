c330125716d69eda5151317e085e3586c703c28b
Paulo <pshenrique2015@gmail.com>
2024-02-10 10:25:36 -0300

    [add]-usuários buscar.

--- src/app/administracao/usuarios-buscar/usuarios-buscar.component.ts
+++ src/app/administracao/usuarios-buscar/usuarios-buscar.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { NexusBuscar } from '../../compartilhado/models/nexus-buscar';
 import { SessaoService } from '../../compartilhado/services/sessao/sessao.service';
-import { LocalizacoesService } from '../../localizacoes/localizacoes.service';
+import { UsuariosService } from '../../login/usuarios.service';
+import { DatePipe } from '@angular/common';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-usuarios-buscar',
@@ -23,9 +25,52 @@ export class UsuariosBuscarComponent extends NexusBuscar {
   constructor(
     snackBar: MatSnackBar,
     sessaoService: SessaoService,
-    service: LocalizacoesService
+    service: UsuariosService
   ) {
     super(snackBar, sessaoService);
     this.service = service;
+  }
+
+  //Override para itens que não são de projetos.
+  override carregarTabela(nomePesquisa?: string | null): void {
+    this.carregando = true;
+    const projetoUID = this.sessaoService.projetoSelecionado.uid;
+
+    if (nomePesquisa != null) {
+      this.tabelaFoiFiltrada = true;
+      this.nomeFiltro = nomePesquisa;
+    }
+
+    this.service.obterTudo(this.paginaAtual + 1, nomePesquisa)
+    .subscribe({
+      next: (dados: any) => {
+        const pipe = new DatePipe('en-US');
+
+        const dadosTratados: any[] = dados.itens;
+        this.totalItens = dados.totalItens;
+      
+        dadosTratados.forEach(dado => {
+          for (const propriedade in dado) {
+            //Se for data, converte para uma string legível.
+            if (this.eData(dado[propriedade])) {
+              dado[propriedade] = pipe
+                .transform(dado[propriedade], 'dd/MM/yyyy HH:mm:ss');
+            }
+    
+            //Se for uma referência, retorna o nome do objeto.
+            if (dado[propriedade] instanceof Object) {
+              dado[propriedade] = dado[propriedade].nome;
+            }
+          }
+        })
+    
+        this.selecionouObjeto = false;
+        this.carregando = false;
+        this.dadosTabela = new MatTableDataSource<any>(dadosTratados);
+      },
+      error: () => {
+        this.mostrarSnackBarOk('Não foi possível carregar as informações.');
+      }
+    });
   }
 }
