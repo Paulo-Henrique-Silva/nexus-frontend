f076bc04c060a224d2770c154736b0d2953db9f7
Paulo <pshenrique2015@gmail.com>
2024-02-11 12:06:12 -0300

    [add]-projeto detalhes.

--- src/app/administracao/projetos-detalhes/projetos-detalhes.component.ts
+++ src/app/administracao/projetos-detalhes/projetos-detalhes.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router, ActivatedRoute } from '@angular/router';
+import { throwError } from 'rxjs';
+import { NexusFormulario } from '../../compartilhado/models/nexus-formulario';
+import { MensagensValidacaoService } from '../../compartilhado/services/mensagens-validacao/mensagens-validacao.service';
+import { SessaoService } from '../../compartilhado/services/sessao/sessao.service';
+import { AuthService } from '../../login/auth/auth.service';
+import { ProjetoService } from '../services/projeto/projeto.service';
 
 @Component({
   selector: 'app-projetos-detalhes',
   templateUrl: './projetos-detalhes.component.html',
   styleUrl: './projetos-detalhes.component.scss'
 })
-export class ProjetosDetalhesComponent {
+export class ProjetosDetalhesComponent extends NexusFormulario {
+  constructor(
+    authService : AuthService, 
+    formBuilder: FormBuilder,
+    router: Router,
+    mensagemValidacaoService: MensagensValidacaoService,
+    activatedRoute: ActivatedRoute,
+    snackBar: MatSnackBar,
+    usuarioSessaoService: SessaoService,
+    private service: ProjetoService
+  ) {
+    super(authService, formBuilder, router, mensagemValidacaoService, activatedRoute, 
+      snackBar, usuarioSessaoService);
 
+    this.formulario = this.formBuilder.group({
+      nome: [''],
+      descricao: [''],
+      atualizadoPor: [''],
+      dataUltimaAtualizacao: [''],
+      usuarioCriador: [''],
+      dataCriacao: ['']
+    });
+  }
+
+  ngOnInit() {
+    this.carregando = true;
+    const uid = this.activatedRoute.snapshot.params['uid'];
+
+    this.service.obterPorUID(uid)
+    .subscribe({
+      next: (projeto) => {
+        if (!projeto) {
+          throwError(() => Error('Objeto não encontrado.'));
+        }
+    
+        this.formulario.setValue({
+          nome: projeto.nome,
+          descricao: projeto.descricao,
+          atualizadoPor: projeto.atualizadoPor?.nome,
+          dataUltimaAtualizacao: projeto.dataUltimaAtualizacao,
+          usuarioCriador: projeto.usuarioCriador?.nome,
+          dataCriacao: projeto.dataCriacao,
+        })
+
+        this.carregando = false;
+      },
+      error: () => this.mostrarSnackBarOk('Um erro inesperado aconteceu!')
+    });
+  }
+  
+  override onSubmit(): void { }
 }
